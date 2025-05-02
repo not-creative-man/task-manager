3836cfad554d2391103adbf721b0dbaf6cf58b77
not-creative-man <lebedev_i_s@inbox.ru>
2025-05-02 09:39:46 +0300

    Switch from rollup to esbuild for frontend build

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
@@ -13,6 +13,13 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
+    }
   },
+  build: {
+    // Используем esbuild вместо rollup
+    minify: 'esbuild',
+    rollupOptions: {
+      external: []
+    }
+  }
 })
