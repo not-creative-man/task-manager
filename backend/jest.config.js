027265112f17fdd20f8dba84f0f2d224fbb3e527
not-creative-man <lebedev_i_s@inbox.ru>
2025-08-29 02:51:48 +0300

    Updated configs for backend tests

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -8,5 +8,14 @@ export default {
   collectCoverage: true,
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
+  coverageReporters: ['text', 'lcov'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  },
   testMatch: ['**/tests/**/*.test.js']
 };
